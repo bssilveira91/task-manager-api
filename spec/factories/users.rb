10b607763d3b4961592bc64004be82e0a92960c0
Bruno S. Silveira <bruno.silveira@brunoss.com.br>
2018-04-17 22:22:54 -0300

    Disable devise routes

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,7 @@
-FactoryGirl.define do
+FactoryBot.define do
     factory :user do
-
+        email "bruno@silveira.com"
+        password "123456"
+        password_confirmation "123456"
     end
-
 end
