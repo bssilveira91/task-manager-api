045e10938a2b0fc378ac1c0754f9520ad5e1262e
Bruno S. Silveira <bruno.silveira@brunoss.com.br>
2018-04-17 22:33:20 -0300

    Adding create action to the user controller

--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -4,20 +4,20 @@ RSpec.describe 'Users API', type: :request do
   let(:user) { create(:user) }
   let(:user_id) { user.id }
 
-  before {host! "api.task-manager.test"}
+  before {host! 'api.task-manager.test'}
 
-  describe "GET users/:id" do
+  describe 'GET users/:id' do
     before do
-      headers = {"Accept" => "application/vnd.taskmanager.v1"}
+      headers = {'Accept' => 'application/vnd.taskmanager.v1'}
 
       get "/users/#{user_id}", {}, headers
     end
 
-    context "When the user exists" do
+    context 'When the user exists' do
       it 'returns the user' do
         user_response = JSON.parse(response.body)
 
-        expect(user_response["id"]).to eq(user_id)
+        expect(user_response['id']).to eq(user_id)
       end
     end
   end
